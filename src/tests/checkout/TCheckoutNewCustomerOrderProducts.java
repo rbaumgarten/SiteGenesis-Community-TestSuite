90b1ea201eedae1b7fb0e54063685aa4c771c4bf
Rene Schwietzke <Rene Schwietzke>
2015-06-07 19:03:40 +0200

    Actions adjusted

--- src/tests/checkout/TCheckoutNewCustomerOrderProducts.java
+++ src/tests/checkout/TCheckoutNewCustomerOrderProducts.java
@@ -32,10 +32,6 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <li>Validate order via order history</li>
  * <li>Logout</li>
  * </ul>
- * <h1 id="3-clean-up">3. Clean up</h1>
- * <ul>
- * <li>End session and clean up created cookies</li>
- * </ul>
  */
 @ScriptName
 ("tests.checkout.TCheckoutNewCustomerOrderProducts")
