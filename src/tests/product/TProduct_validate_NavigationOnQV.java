19f59d8b708b756223fab08ca65084256a47a71e
Timo Naumann <t.naumann@demandware.com>
2015-06-10 17:51:22 +0200

    created new testcase and set parameters for it

---
+++ src/tests/product/TProduct_validate_NavigationOnQV.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.product;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("tests.product.TProduct_validate_NavigationOnQV")
+public class TProduct_validate_NavigationOnQV extends AbstractScriptTestCase
+{
+}
