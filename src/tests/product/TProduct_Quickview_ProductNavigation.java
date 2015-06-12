abc7ea2cc88196adfc2106ece589220dc84e7927
Rene Schwietzke <Rene Schwietzke>
2015-06-12 16:32:25 +0200

    Renamed test case again, because it is functionality we test not just plain validation

--- src/tests/product/TProduct_Quickview_ProductNavigation.java
+++ src/tests/product/TProduct_Quickview_ProductNavigation.java
@@ -7,22 +7,22 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Verifies the product browsing via quickview (QV)</p>
+ * <p>Verifies product navigation on the quickview</p>
  * <h1 id="setup">Setup</h1>
  * <p>Open the site and remove cookies</p>
  * <h1 id="scope">Scope</h1>
- * <p>Validate the funion of the next and prev button in the quickview header.</p>
+ * <p>Validate the previous and next functionality of the quickview</p>
  * <ul>
  * <li>open first product</li>
  * <li>browse to last product, while saving the product names of the seen products</li>
  * <li>browse back to first product and verify if the saved product names are seen again</li>
- * <li>close Quickview</li>
- * <li>reopen the last product and verify the product name again</li>
+ * <li>close quickview</li>
+ * <li>reopen the last product and verify the product name</li>
  * <li>close quickview again</li>
  * </ul>
  */
 @ScriptName
-("tests.product.TProduct_Validate_NavigationOnQV")
-public class TProduct_Validate_NavigationOnQV extends AbstractScriptTestCase
+("tests.product.TProduct_Quickview_ProductNavigation")
+public class TProduct_Quickview_ProductNavigation extends AbstractScriptTestCase
 {
 }
