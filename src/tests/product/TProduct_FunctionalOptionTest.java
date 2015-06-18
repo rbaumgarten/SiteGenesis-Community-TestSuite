94c99c0668b5011dfa41e955823a32848ec8a4ef
Timo Naumann <t.naumann@demandware.com>
2015-06-18 13:00:23 +0200

    change Option test case to make it nice added commtis

--- src/tests/product/TProduct_FunctionalOptionTest.java
+++ src/tests/product/TProduct_FunctionalOptionTest.java
@@ -7,17 +7,15 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Verifies the structure of quickview (QV) and product details pages (PDP) for variation products</p>
+ * <p>Verifies the function of an option product on quickview (QV) and product details pages (PDP) </p>
  * <h1 id="setup">Setup</h1>
  * <p>Open the site and remove cookies</p>
  * <h1 id="scope">Scope</h1>
- * <p>Validate displaying product data either via quick view or via product detail page.</p>
+ * <p>Validate correct funtion of the option field </p>
  * <ul>
- * <li>validate structure of the QV</li>
- * <li>go from QV to PDP</li>
- * <li>validate stucture of PDP</li>
- * <li>check if product name, price, and product number are the same on both </li>
- * <li>search for product name and validate PDP again</li>
+ * <li>validate function on QV</li>
+ * <li>go to pdp</li>
+ * <li>validate funtion on PDP</li>
  * </ul>
  */
 @ScriptName
