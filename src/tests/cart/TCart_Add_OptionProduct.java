94d145e5b0663a3505435fb4dc1b1ff8736e6858
Timo Naumann <t.naumann@demandware.com>
2015-06-24 18:11:48 +0200

    created new testcases for addtocart from QV and PDP

---
+++ src/tests/cart/TCart_Add_OptionProduct.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test adds a Option Product with selected Option to the cart from QV and PDP</p>
+ */
+@ScriptName
+("tests.cart.TCart_Add_OptionProduct")
+public class TCart_Add_OptionProduct extends AbstractScriptTestCase
+{
+}
---
+++ src/tests/cart/TCart_Add_StandartProduct.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test adds a Standart Product  to the cart from QV and PDP</p>
+ */
+@ScriptName
+("tests.cart.TCart_Add_StandartProduct")
+public class TCart_Add_StandartProduct extends AbstractScriptTestCase
+{
+}
