ac0b1891f69e4903b383ed744e436358333ae5ef
Rene Schwietzke <Rene Schwietzke>
2015-06-24 21:39:16 +0200

    Renamed test case

--- src/tests/product/TProduct_Validate_OptionProductFunctionality.java
+++ src/tests/product/TProduct_Validate_OptionProductFunctionality.java
@@ -19,7 +19,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * </ul>
  */
 @ScriptName
-("tests.product.TProduct_Functional_OptionProduct")
-public class TProduct_Functional_OptionProduct extends AbstractScriptTestCase
+("tests.product.TProduct_Validate_OptionProductFunctionality")
+public class TProduct_Validate_OptionProductFunctionality extends AbstractScriptTestCase
 {
 }
