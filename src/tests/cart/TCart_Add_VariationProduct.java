c0b149f4b4efba55477d1efb89e225b2e4cf3b6d
Timo Naumann <t.naumann@demandware.com>
2015-06-25 14:23:21 +0200

    created new Testcase for Variationproducts

---
+++ src/tests/cart/TCart_Add_VariationProduct.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test adds a Variation Product  to the cart from QV and PDP</p>
+ */
+@ScriptName
+("tests.cart.TCart_Add_VariationProduct")
+public class TCart_Add_VariationProduct extends AbstractScriptTestCase
+{
+}
