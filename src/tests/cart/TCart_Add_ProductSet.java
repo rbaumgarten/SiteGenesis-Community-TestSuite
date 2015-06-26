06e832dce8a260b8717f134744568997d4ac0fd2
Timo Naumann <t.naumann@demandware.com>
2015-06-26 17:17:45 +0200

    created add-to-cart test for product set

---
+++ src/tests/cart/TCart_Add_ProductSet.java
@@ -0,0 +1,30 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test adding a standard product to the cart from quickview and product detail pages.</p>
+ * <h1 id="setup">Setup</h1>
+ * <ul>
+ * <li>open the session</li>
+ * <li>search for possible products</li>
+ * <li>open quickview</li>
+ * </ul>
+ * <h1 id="scope">Scope</h1>
+ * <ul>
+ * <li>add to cart from quickview</li>
+ * <li>verify cart</li>
+ * <li>remove product from cart</li>
+ * <li>go to product and add to cart from product detail page</li>
+ * <li>verify cart</li>
+ * </ul>
+ */
+@ScriptName
+("tests.cart.TCart_Add_ProductSet")
+public class TCart_Add_ProductSet extends AbstractScriptTestCase
+{
+}
