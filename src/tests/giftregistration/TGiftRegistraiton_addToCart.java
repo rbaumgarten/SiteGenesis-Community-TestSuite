888b203c5548336adf46bb4f2f1e9ce5c7f95685
Timo Naumann <t.naumann@demandware.com>
2015-07-17 10:41:14 +0200

    created new add gift certificate to cart teyst

---
+++ src/tests/giftregistration/TGiftRegistraiton_addToCart.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.giftregistration;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("tests.giftregistration.TGiftRegistraiton_addToCart")
+public class TGiftRegistraiton_addToCart extends AbstractScriptTestCase
+{
+}
