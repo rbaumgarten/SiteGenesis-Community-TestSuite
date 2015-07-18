49de62b7122df9e7f85a2de8736ef4ddc1c3bbe9
Rene Schwietzke <Rene Schwietzke>
2015-07-18 20:59:25 +0200

    Actions and comments fixed

--- src/tests/wishlist/TWishList_UseWishListForAddToCart.java
+++ src/tests/wishlist/TWishList_UseWishListForAddToCart.java
@@ -7,28 +7,21 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Test add product to wishlist.</p>
+ * <p>Adds a product to the cart from a wishlist</p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
  * <li>Create new account</li>
- * <li>Search product 01</li>
+ * <li>Search product</li>
+ * <li>add it to the wishlist</li>
+ * <li>logout</li>
  * </ul>
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
- * <li>Store product details and add to wishlist</li>
  * <li>Login</li>
- * <li>Validate nav and page</li>
- * <li>Add product to wishlist</li>
- * <li>Validate product basic details</li>
- * <li>Remove product </li>
- * <li>Validate empty</li>
- * <li>do it again for QV</li>
- * </ul>
- * <h1 id="3-clean-up">3. Clean up</h1>
- * <ul>
+ * <li>Access wishlist</li>
+ * <li>add product in wishlist to cart</li>
  * <li>Logout</li>
- * <li>End session and clean up created cookies</li>
  * </ul>
  */
 @ScriptName
