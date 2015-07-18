eff689dc2983b8902d90e8eda423b34e0a76fa72
Rene Schwietzke <Rene Schwietzke>
2015-07-18 21:33:44 +0200

    Actions and documentation adjusted

--- src/tests/wishlist/TWishList_SendWishListToFriend.java
+++ src/tests/wishlist/TWishList_SendWishListToFriend.java
@@ -7,20 +7,27 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Test add product to wishlist.</p>
+ * <p>Send to wishlist to a friend. Does not validate the correctness of the sent email, only the success message is checked.</p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
- * <li>Start session, open homepage and delete all visible cookies</li>
- * <li>Create new account</li>
- * <li>Search product 01</li>
+ * <li>create sender and receiver email</li>
+ * <li>create sender account password</li>
+ * <li>start session</li>
+ * <li><p>create a new account but do not log out</p>
+ * </li>
+ * <li><p>search product</p>
+ * </li>
  * </ul>
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
- * <li>add product to wishlist</li>
- * <li>click send to friend </li>
- * <li>cancel form</li>
- * <li>click send to friend</li>
- * <li>click preview</li>
+ * <li>open send wishlist to friend dialog</li>
+ * <li>cancel dialog</li>
+ * <li>open send wishlist to friend dialog again</li>
+ * <li>fill out form, preview mail</li>
+ * <li>send form data</li>
+ * <li>close dialog</li>
+ * <li>repeat but do not use preview, send immediately</li>
+ * <li>logou</li>
  * </ul>
  */
 @ScriptName
