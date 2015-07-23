550bbbf2aad6648eaa882b8f92fef1f0eefd5cc4
Timo Naumann <t.naumann@demandware.com>
2015-07-23 18:20:10 +0200

    move some scripts

--- src/tests/giftcertificates/TGiftCertificates_buyGiftCard.java
+++ src/tests/giftcertificates/TGiftCertificates_buyGiftCard.java
@@ -9,8 +9,9 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 /**
  * <h1 id="setup">Setup</h1>
  * <ul>
- * <li>create an email for the receipient</li>
+ * <li>create an email for the receipient,with a short time mail program</li>
  * <li>open the homepage and start the session</li>
+ * <li>create an account for the buyer</li>
  * </ul>
  * <h1 id="scope">Scope</h1>
  * <ul>
@@ -18,6 +19,9 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <li>add it to the cart</li>
  * <li>go to the cart</li>
  * <li>validate the gift cert in the cart</li>
+ * <li>purchase gift cart</li>
+ * <li>validates is gift card sended</li>
+ * <li>check balance of gift card in the storefront</li>
  * </ul>
  */
 @ScriptName
