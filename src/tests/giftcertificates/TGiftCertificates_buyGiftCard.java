f129a2f5172edcd1c51881c5a078c7f409e22d27
Timo Naumann <t.naumann@demandware.com>
2015-07-23 18:17:18 +0200

    created new test case for purchasing a giftcard

---
+++ src/tests/giftcertificates/TGiftCertificates_buyGiftCard.java
@@ -0,0 +1,27 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.giftcertificates;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <h1 id="setup">Setup</h1>
+ * <ul>
+ * <li>create an email for the receipient</li>
+ * <li>open the homepage and start the session</li>
+ * </ul>
+ * <h1 id="scope">Scope</h1>
+ * <ul>
+ * <li>go to gift certificates page and validate it</li>
+ * <li>add it to the cart</li>
+ * <li>go to the cart</li>
+ * <li>validate the gift cert in the cart</li>
+ * </ul>
+ */
+@ScriptName
+("tests.giftcertificates.TGiftCertificates_buyGiftCard")
+public class TGiftCertificates_buyGiftCard extends AbstractScriptTestCase
+{
+}
