4f7a5a8193b019db2d74902775da11652eefa5ba
Rene Schwietzke <Rene Schwietzke>
2015-07-24 17:27:54 +0200

    Final test data set and small fixes

--- src/tests/checkout/TCheckoutOrder_AsGuest_DifferentBillingAddress.java
+++ src/tests/checkout/TCheckoutOrder_AsGuest_DifferentBillingAddress.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Test guest checkout with one variation product with color and size, one address and VISA. </p>
+ * <p>Test guest checkout with one variation product with color and size and VISA. Uses a different address for shipping than billing. Besides that it is identically to TCheckoutOrder_AsGuest.</p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
@@ -16,15 +16,15 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
  * <li>Start guest checkout</li>
- * <li>Enter new address and use preselected  default shipping method</li>
- * <li>Use different addresss for billing and shipping use payment method credit card (visa)</li>
+ * <li>Enter new address and use preselected default shipping method</li>
+ * <li>Use different addresss for billing and use payment method credit card (visa)</li>
  * <li>Validate place order page</li>
  * <li>Validate order confirmation page</li>
  * <li>Create an account</li>
  * <li>Validate customer name and profile</li>
  * <li>Validate order via order history</li>
  * </ul>
- * <p>Note: This test is prepared as data driven test. Running it as junit test will execute the test multiple times using the set of test data in the _datasets.xml file. The configured example places orders with different types of credit cards.</p>
+ * <p>Note: This test is prepared as data driven test. Running it as junit test will execute the test multiple times using the set of test data in the _datasets.xml file. The configured example places orders with different cart quantities and combinations of addresses.</p>
  */
 @ScriptName
 ("tests.checkout.TCheckoutOrder_AsGuest_DifferentBillingAddress")
