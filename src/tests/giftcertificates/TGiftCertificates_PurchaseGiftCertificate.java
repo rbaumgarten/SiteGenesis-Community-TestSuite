07c505e314e142537aa0e58a8557f4057a52151e
Rene Schwietzke <Rene Schwietzke>
2015-07-25 17:25:15 +0200

    Polishing

--- src/tests/giftcertificates/TGiftCertificates_PurchaseGiftCertificate.java
+++ src/tests/giftcertificates/TGiftCertificates_PurchaseGiftCertificate.java
@@ -9,19 +9,19 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 /**
  * <h1 id="setup">Setup</h1>
  * <ul>
- * <li>create an email for the receipient,with a short time mail program</li>
+ * <li>create an email for the receipient using an external service</li>
  * <li>open the homepage and start the session</li>
  * <li>create an account for the buyer</li>
  * </ul>
  * <h1 id="scope">Scope</h1>
  * <ul>
  * <li>go to gift certificates page and validate it</li>
- * <li>add it to the cart</li>
+ * <li>add a gift certificate to the cart</li>
  * <li>go to the cart</li>
- * <li>validate the gift cert in the cart</li>
- * <li>purchase gift cart</li>
- * <li>validates is gift card sended</li>
- * <li>check balance of gift card in the storefront</li>
+ * <li>validate the gift certificate in the cart</li>
+ * <li>purchase cart</li>
+ * <li>validate if gift certificate was sent</li>
+ * <li>check balance of gift certificates</li>
  * </ul>
  */
 @ScriptName
