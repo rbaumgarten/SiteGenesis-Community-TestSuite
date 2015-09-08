7ff726b9eafa242991ca55fd3350b44b6bd348b4
Timo Naumann <t.naumann@demandware.com>
2015-09-08 16:10:29 +0200

    make some changes for 15.9

--- src/tests/cart/TCart_Add_OptionProduct.java
+++ src/tests/cart/TCart_Add_OptionProduct.java
@@ -7,24 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Add an option product to the cart.</p>
- * <h1 id="setup">Setup</h1>
- * <ul>
- * <li>Start session, open homepage and delete all visible cookies.</li>
- * </ul>
- * <h1 id="scope">Scope</h1>
- * <ul>
- * <li>search product</li>
- * <li>sort to ensure consitency</li>
- * <li><p>open quckview, select option, add to cart</p>
- * </li>
- * <li><p>verify cart</p>
- * </li>
- * <li><p>remove product again</p>
- * </li>
- * <li><p>repeat with add to cart from product detail page</p>
- * </li>
- * </ul>
+ * <p>Test adds a Option Product with selected Option to the cart from QV and PDP</p>
  */
 @ScriptName
 ("tests.cart.TCart_Add_OptionProduct")
