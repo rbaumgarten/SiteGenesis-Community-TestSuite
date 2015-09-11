9ec9f16fe16db103ff3cda0496cc1b7b012eb2cf
Timo Naumann <t.naumann@demandware.com>
2015-09-11 10:14:20 +0200

    minor fixes

---
+++ src/tests/cart/TCart_Update_OptionProductInCart.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test adds a Option Product with selected Option to the cart from PDP and updates the option in the cart</p>
+ */
+@ScriptName
+("tests.cart.TCart_Update_OptionProductInCart")
+public class TCart_Update_OptionProductInCart extends AbstractScriptTestCase
+{
+}
