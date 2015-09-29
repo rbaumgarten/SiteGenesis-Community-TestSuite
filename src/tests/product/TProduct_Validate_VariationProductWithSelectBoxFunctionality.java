78008e30d03c6edcc27a3bc30281a9b37e2a72e2
Timo Naumann <t.naumann@demandware.com>
2015-09-29 12:11:55 +0200

    polished documentation

--- src/tests/product/TProduct_Validate_VariationProductWithSelectBoxFunctionality.java
+++ src/tests/product/TProduct_Validate_VariationProductWithSelectBoxFunctionality.java
@@ -11,11 +11,20 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <h1 id="setup">Setup</h1>
  * <p>Open the site and remove cookies</p>
  * <h1 id="scope">Scope</h1>
- * <p>Validates the change of the product numbers and enabling of the add2cart button when changing color and size, checks also if the availability changes</p>
+ * <p>Validates the change of the product numbers and enabling of the add2cart button when changing the size, checks also if the availability changes</p>
+ * <h2 id="qv">QV</h2>
  * <ul>
  * <li>open QV</li>
- * <li>change color, verify that the product number changes</li>
- * <li>select a size, verify that the add2cart button becomes enabled</li>
+ * <li>change size, verify if price and product nuber are changed</li>
+ * <li>try to select default option</li>
+ * <li>change to pdp</li>
+ * </ul>
+ * <h2 id="pdp">PDP</h2>
+ * <ul>
+ * <li>change selected option validate if price and procuct nuber changes</li>
+ * <li>try to select default option</li>
+ * <li>search to get the variation master page</li>
+ * <li>verify variation master on pdp</li>
  * </ul>
  */
 @ScriptName
