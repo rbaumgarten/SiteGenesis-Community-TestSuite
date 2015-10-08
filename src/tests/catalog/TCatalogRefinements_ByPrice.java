365f6f6550b365f8e8ad20df6b842ae674090467
Timo Naumann <t.naumann@demandware.com>
2015-10-08 17:01:36 +0200

    modified modules description and some of the comments

--- src/tests/catalog/TCatalogRefinements_ByPrice.java
+++ src/tests/catalog/TCatalogRefinements_ByPrice.java
@@ -15,16 +15,12 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
  * <li>Browse to category</li>
- * <li>Select, validate and remove color via name</li>
- * <li>Select three colors via position</li>
- * <li>Remove one color via breadcrumb</li>
- * <li>Remove two colors clear</li>
- * <li>Validate empty color list</li>
+ * <li>Select, validate and remove price via tile</li>
+ * <li>Select three prices via title</li>
+ * <li>Remove price via breadcrumb</li>
+ * <li>Remove all prices</li>
+ * <li>Validate empty price list</li>
  * <li>Validate empty refinement list in breadcrumbs</li>
- * </ul>
- * <h1 id="3-clean-up">3. Clean up</h1>
- * <ul>
- * <li>End session and clean up created cookies</li>
  * </ul>
  */
 @ScriptName
