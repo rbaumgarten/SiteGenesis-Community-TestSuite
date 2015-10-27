952fb36a2326a2df6c039527ae05732b13fca765
Robert Baumgarten <r.baumgarten@xceptance.de>
2015-10-27 08:29:08 +0100

    added rap number to description, updated tags

--- src/tests/product/TProduct_Validate_VariationProductDropdownAndColorFunctionality.java
+++ src/tests/product/TProduct_Validate_VariationProductDropdownAndColorFunctionality.java
@@ -7,7 +7,7 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Verifies if a value of a variation dropdown can be changed after selecting a color. It retests a known issue.</p>
+ * <p>Verifies if a value of a variation dropdown can be changed after selecting a color. It retests a known issue (RAP-4413).</p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
