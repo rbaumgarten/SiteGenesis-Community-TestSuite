40106bb0ee39b359d5b591e45219928ff6d97e15
Timo Naumann <t.naumann@demandware.com>
2015-10-27 11:56:17 +0100

    created first half of testcase

---
+++ src/tests/catalog/TCatalogProductListing_SortByPrice.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.catalog;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("tests.catalog.TCatalogProductListing_SortByPrice")
+public class TCatalogProductListing_SortByPrice extends AbstractScriptTestCase
+{
+}
---
+++ src/tests/promotion/TPromotion_free2DayShipping.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.promotion;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("tests.promotion.TPromotion_free2DayShipping")
+public class TPromotion_free2DayShipping extends AbstractScriptTestCase
+{
+}
