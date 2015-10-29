22f88b916ff6129cba392caca583f2946daacb6d
Timo Naumann <t.naumann@demandware.com>
2015-10-29 14:49:21 +0100

    created new testcase TPromotion_buy5for50

---
+++ src/tests/promotion/TPromotion_buy5for50.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.promotion;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * 
+ */
+@ScriptName
+("tests.promotion.TPromotion_buy5for50")
+public class TPromotion_buy5for50 extends AbstractScriptTestCase
+{
+}
