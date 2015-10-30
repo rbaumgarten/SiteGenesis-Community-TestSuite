4c996bd9e020583bd004150ddd317d22ddaf5b94
Timo Naumann <t.naumann@demandware.com>
2015-10-30 15:57:34 +0100

    added testcase description

--- src/tests/promotion/TPromotion_buy5for50.java
+++ src/tests/promotion/TPromotion_buy5for50.java
@@ -7,7 +7,23 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * 
+ * <p>Verifies the functionality of an buy5for50 promotion.</p>
+ * <h1 id="setup">Setup</h1>
+ * <p>Open the site and remove cookies
+ * Put four promotion product in cart
+ * Validate that no promotion is shown</p>
+ * <h1 id="scope">Scope</h1>
+ * <p>Validate correct funtionality of the promotion</p>
+ * <ul>
+ * <li>change quanitiy to five</li>
+ * <li>validate that promotion is shown correct</li>
+ * <li>change quanitiy to six</li>
+ * <li>validate that promotion is shown correct </li>
+ * <li>change quanitiy to ten</li>
+ * <li>validate that promotion is shown correct</li>
+ * <li>change quanitiy back to 4</li>
+ * <li>validate that no promotion is shown</li>
+ * </ul>
  */
 @ScriptName
 ("tests.promotion.TPromotion_buy5for50")
