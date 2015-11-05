9cd7197f1db53c833da7c6430223b46d6b70bcef
Robert Baumgarten <r.baumgarten@xceptance.de>
2015-11-05 16:05:08 +0100

    updated description

--- src/tests/catalog/TCatalogProductListing_SortByPrice.java
+++ src/tests/catalog/TCatalogProductListing_SortByPrice.java
@@ -7,28 +7,24 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Test validate the Sort By Price Rule</p>
+ * <p>(JAVA)-Test sort by price.  </p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
- * <li>search for a product set</li>
- * <li>show all results on one page</li>
+ * <li>Search for products</li>
+ * <li>Select items per page randomly</li>
  * </ul>
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
- * <li>Sort By Price - low to high</li>
- * <li>validate that the first price is smaller than the second one</li>
- * <li>validate that the penultimate price is smaller than the last one</li>
- * <li><p>validate that the second price is smaller than the penultimate</p>
- * </li>
- * <li><p>Sort By Price - High To Low</p>
- * </li>
- * <li>validate that Highest Price is now the first one</li>
- * <li><p>validate that smalles price is now the last one</p>
- * </li>
- * <li><p>Sort By Price - Low To High</p>
- * </li>
- * <li>validat that highest price is the last one and lowest is the first one </li>
+ * <li>Sort by price low to high</li>
+ * <li>Validate sorting order </li>
+ * <li>Sort by price high to low</li>
+ * <li>Validate sorting order </li>
+ * </ul>
+ * <p>Notes:</p>
+ * <ul>
+ * <li>Validating sorting order is implemented as java module which is not executed in script developer. </li>
+ * <li>There is a data set configured for multiple data driven executions via JUnit.</li>
  * </ul>
  */
 @ScriptName
